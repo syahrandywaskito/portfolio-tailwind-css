3059ce3654c10640385906ffd1fc5d35bea37ef6
Syahrandy Waskito <syahrandywaskito666@gmail.com>
2024-01-15 17:40:26 +0700

    Update : portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "25px",
+    },
+    extend: {
+      colors: {
+        primary: "#6d28d9",
+        dark: "#0f172a",
+        secondary: "#64748b",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+      fontFamily: {
+        monobuntu: "Ubuntu Mono",
+      }
+    },
+  },
+  plugins: [],
+};
+
